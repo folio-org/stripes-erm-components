e08f8ff66182af67cc0a9b264a5b08e91db1afd6
Mark Deutsch <mark.deutsch@duke.edu>
2019-02-27 12:00:00 -0500

    Added DocumentsFieldArray component

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export { default as CreateOrganizationModal } from './lib/CreateOrganizationModal';
 export { default as DocumentCard } from './lib/DocumentCard';
+export { default as DocumentsFieldArray } from './lib/DocumentsFieldArray';
 export { default as OrganizationSelection } from './lib/OrganizationSelection';
 export { default as Spinner } from './lib/Spinner';
 
