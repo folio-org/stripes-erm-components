13a94fe66c23dd951301ea560f504f3a5538b0a1
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-03-14 13:31:37 +0100

    erm-96: add lib/LicenseTermsList create LicenseTermsList.js, index.js, readme.md modify index.js, CHANGELOG.md

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ export { default as DocumentCard } from './lib/DocumentCard';
 export { default as DocumentsFieldArray } from './lib/DocumentsFieldArray';
 export { default as LicenseCard } from './lib/LicenseCard';
 export { default as LicenseEndDate } from './lib/LicenseEndDate';
+export { default as LicenseTermsList } from './lib/LicenseTermsList';
 export { default as OrganizationSelection } from './lib/OrganizationSelection';
 export { default as Spinner } from './lib/Spinner';
 
---
+++ lib/LicenseTermsList/readme.md
@@ -0,0 +1,16 @@
+# LicenseTermsList
+
+
+
+
+## Basic Usage
+Simply passing a license object is all that's required.
+```
+
+```
+
+## Props
+
+| Name | Type | Description | Required | Default |
+--- | --- | --- | --- | --- |
+| `license` | object | [`License`](https://github.com/folio-org/mod-licenses/blob/master/service/grails-app/domain/org/olf/licenses/License.groovy) | Yes |
