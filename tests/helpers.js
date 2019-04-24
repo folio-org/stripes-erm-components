fb1cd44b01d9886fa0fa2052a3133fd851175ee6
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-04-24 10:56:54 -0400

    big tests setup for erm smart components, refs ERM-199

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,37 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Harness from './Harness';
+import { beforeEach } from '@bigtest/mocha';
+import setupStripesCore from '@folio/stripes-core/test'
+import { withModules, clearModules } from '@folio/stripes-core/test/bigtest/helpers/stripes-config';
+import mirageOptions from './network';
 
-function getCleanTestingRoot() {
-  let $root = document.getElementById('root');
+export function setupApplication({
+  scenarios
+} = {}) {
+  setupStripesCore({
+    mirageOptions,
+    scenarios,
 
-  // if a root exists, unmount anything inside and remove it
-  if ($root) {
-    ReactDOM.unmountComponentAtNode($root);
-    $root.parentNode.removeChild($root);
-  }
+    // setup a dummy app for smart components
+    modules: [{
+      type: 'app',
+      name: '@folio/ui-dummy',
+      displayName: 'dummy.title',
+      route: '/dummy',
+      module: null
+    }],
 
-  // create a brand new root element
-  $root = document.createElement('div');
-  $root.id = 'root';
+    translations: {
+      'dummy.title': 'Dummy'
+    }
+  });
 
-  document.body.appendChild($root);
-
-  return $root;
+  // go to the dummy app where smart components are mounted
+  beforeEach(function () { // eslint-disable-line func-names
+    this.visit('/dummy');
+  });
 }
 
+// replace the dummy app to mount the component
 export function mount(component) {
-  return new Promise(resolve => {
-    ReactDOM.render(component, getCleanTestingRoot(), resolve);
-  });
-}
+  clearModules();
 
-export function mountWithContext(component) {
-  return new Promise(resolve => {
-    ReactDOM.render(<Harness>{component}</Harness>, getCleanTestingRoot(), resolve);
-  });
-}
-
-export function selectorFromClassnameString(str) {
-  return str.replace(/\s/, '.');
+  withModules([{
+    type: 'app',
+    name: '@folio/ui-dummy',
+    displayName: 'dummy.title',
+    route: '/dummy',
+    module: () => component
+  }]);
 }
