c1caad05a340127263cc3116fb2bb653e16f915c
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-05-02 14:58:56 -0400

    remove describe.only, refs ERM-199

--- tests/network/factories/organization.js
+++ tests/network/factories/organization.js
@@ -3,6 +3,6 @@ import { Factory, faker } from '@bigtest/mirage';
 export default Factory.extend({
   id: () => faker.random.uuid(),
   vendorsUuid: () => faker.random.uuid(),
-  name: () => faker.list.cycle('Economics', 'Philosophy', 'English', 'History', 'Mathematics'),
+  name: faker.list.cycle('Economics', 'Philosophy', 'English', 'History', 'Mathematics'),
   reference: () => faker.random.word(),
 });
