9b0bd96c59346d38f04a196213c00f9ed49c9b09
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-05-02 13:37:19 -0400

    BigTests for CreateOrganizationModal, refs ERM-199

--- tests/network/config.js
+++ tests/network/config.js
@@ -7,4 +7,14 @@ export default function config() {
       return organizations.all();
     }
   });
+
+  this.get('/erm/org', ({ organizations }, request) => {
+    const { term } = request.queryParams;
+    return organizations.where(org => org.name.toLowerCase().includes(term.toLowerCase()));
+  });
+
+  this.post('/erm/org', (_, request) => {
+    const body = JSON.parse(request.requestBody);
+    return this.create('organization', body);
+  });
 }
--- tests/network/serializers/organization.js
+++ tests/network/serializers/organization.js
@@ -2,7 +2,15 @@ import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
   serialize(...args) {
+    const request = args[1];
     const json = ApplicationSerializer.prototype.serialize.apply(this, args);
-    return json.organizations;
+    if (request.method === 'GET') {
+      return json.organizations;
+    } else if (request.method === 'POST') {
+      const { organization: { id, name } } = json;
+      return { id, name };
+    } else {
+      return json;
+    }
   }
 });
