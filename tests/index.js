66a01f9616ef8ca0772c1c426ec575bcd2cb9c68
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-05-02 16:46:01 -0400

    lint, refs ERM-199

--- tests/index.js
+++ tests/index.js
@@ -3,4 +3,3 @@ require('babel-polyfill');
 // require all test files matching 'lib/**/tests/*-test'
 const requireTest = require.context('../lib/', true, /(.*?)\/tests\/(.*?)-test/);
 requireTest.keys().forEach(requireTest);
-
