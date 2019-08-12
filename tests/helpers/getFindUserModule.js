c398b61fa00074efc9e12cb516ac5eea87bdbd00
Mark Deutsch <mark.deutsch@duke.edu>
2019-08-12 12:33:23 -0400

    clean up test helpers and ICFA tests
    - Moved helpers into their own directory
    - Added tests for ICFA rework

---
+++ tests/helpers/getFindUserModule.js
@@ -0,0 +1,7 @@
+export default (Module) => ({
+  type: 'plugin',
+  name: '@folio/ui-plugin-dummy',
+  displayName: 'dummy.title',
+  pluginType: 'find-user',
+  module: Module
+});
---
+++ tests/helpers/index.js
@@ -0,0 +1,8 @@
+export { dummyMount } from './dummyMount';
+export { getCleanTestingRoot } from './getCleanTestingRoot';
+export { default as getFindUserModule } from './getFindUserModule';
+export { default as Harness } from './Harness';
+export { mount } from './mount';
+export { mountWithContext } from './mountWithContext';
+export { selectorFromClassnameString } from './selectorFromClassnameString';
+export { setupApplication } from './setupApplication';
