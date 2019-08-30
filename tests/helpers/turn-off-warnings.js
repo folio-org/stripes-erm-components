c05367abd883988acb58ffeae9a37c39396b83b8
Mark Deutsch <mark.deutsch@duke.edu>
2019-08-30 14:00:21 -0400

    Turn off warnings in tests

---
+++ tests/helpers/turn-off-warnings.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+const warn = console.warn;
+const blacklist = [
+  /componentWillReceiveProps has been renamed/,
+  /componentWillUpdate has been renamed/,
+  /componentWillMount has been renamed/,
+];
+
+export default function turnOffWarnings() {
+  console.warn = function (...args) {
+    if (blacklist.some(rx => rx.test(args[0]))) {
+      return;
+    }
+    warn.apply(console, args);
+  };
+}
