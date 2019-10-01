a8a3210d6724f9d85f0d05976f664b45dbfdb76d
Mark Deutsch <mark.deutsch@duke.edu>
2019-10-01 14:09:29 -0400

    Fixed tests

--- tests/TestForm.js
+++ tests/TestForm.js
@@ -1,23 +1,32 @@
 /*
-  Basic harness prop for testing redux-form functionality
+  Basic harness prop for testing stripes-final-form functionality
   children should be a <Field> component with the tested component passed
   as the 'component' prop.
 */
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { reduxForm } from 'redux-form';
+import { Form } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
 
-class TestForm extends React.Component {
+
+export default class TestForm extends React.Component {
   static propTypes = {
     children: PropTypes.node
   };
 
   render() {
-    return this.props.children;
+    return (
+      <Form
+        onSubmit={() => {}}
+        mutators={arrayMutators}
+      >
+        {props => (
+          <form onSubmit={props.onSubmit}>
+            {this.props.children}
+          </form>
+        )}
+      </Form>
+    );
   }
 }
-
-export default reduxForm({
-  form: 'formsDemo'
-})(TestForm);
