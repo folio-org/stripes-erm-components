f47d1f98e86136dc46e07295d17c2344b9308761
aditya matukumalli <aditya.matukumalli@gmail.com>
2020-11-11 13:09:15 -0500

    Setup RTL and jest infrastructure, refs ERM-1216

---
+++ test/jest/__mock__/index.js
@@ -0,0 +1,6 @@
+import './currencyData.mock';
+import './reactIntl.mock';
+import './stripesConfig.mock';
+import './stripesCore.mock';
+import './stripesIcon.mock';
+import './stripesSmartComponents.mock';
---
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+jest.mock('@folio/stripes/smart-components', () => ({
+  ...jest.requireActual('@folio/stripes/smart-components'),
+  LocationLookup: () => <div>LocationLookup</div>,
+  ViewMetaData: () => <div>ViewMetaData</div>,
+}), { virtual: true });
