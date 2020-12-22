904072c541e0813bb0d42a65f7c3c8f79673fc5d
aditya matukumalli <aditya.matukumalli@gmail.com>
2020-12-22 12:57:10 -0500

    Add TestForm helper

---
+++ test/jest/helpers/TestForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
+import { Button } from '@folio/stripes/components';
+
+const TestForm = ({
+    children,
+    initialValues,
+    onSubmit,
+    handleSubmit
+}) => {
+    return (
+      <Form
+        initialValues={initialValues}
+        mutators={arrayMutators}
+        onSubmit={onSubmit}
+      >
+        {props => (
+          <form onSubmit={handleSubmit}>
+            {children}
+            <Button id="submit" type="submit">
+              Submit
+            </Button>
+          </form>
+        )}
+      </Form>
+    );
+}
+
+export default TestForm;
