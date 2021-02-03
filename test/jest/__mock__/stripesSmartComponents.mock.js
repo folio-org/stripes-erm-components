6ad68f3abe354e1f82af71747b98f613e0240a6d
aditya matukumalli <aditya.matukumalli@gmail.com>
2021-02-03 18:37:53 -0500

    cleanup

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -4,12 +4,11 @@ jest.mock('@folio/stripes/smart-components', () => ({
   ...jest.requireActual('@folio/stripes/smart-components'),
   LocationLookup: () => <div>LocationLookup</div>,
   ViewMetaData: () => <div>ViewMetaData</div>,
-  StripesConnectedSource(props, logger, resourceName) {
-    const fetchMore = jest.fn(val => val);
-    const totalCount = () => props?.resources?.[resourceName]?.other?.totalRecords ?? undefined;
-    const update = jest.fn();
-    const loaded = jest.fn();
-    const records = () => props?.resources?.[resourceName]?.records ?? [];
-    return { fetchMore, totalCount, update, loaded, records };
-  }
+  StripesConnectedSource: (props, logger, resourceName) => ({
+    fetchMore: jest.fn(val => val),
+    totalCount: () => props?.resources?.[resourceName]?.other?.totalRecords ?? undefined,
+    update: jest.fn(),
+    loaded: jest.fn(),
+    records: () => props?.resources?.[resourceName]?.records ?? []
+  })
 }), { virtual: true });
