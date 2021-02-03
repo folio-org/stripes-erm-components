3fb779f7dcf6bce06614e53d2cc790225f059b01
aditya matukumalli <aditya.matukumalli@gmail.com>
2021-02-03 18:56:11 -0500

    Update stripes mock (#349)
    * Add stripesConnectedSource mocks
    
    * cleanup

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -4,4 +4,11 @@ jest.mock('@folio/stripes/smart-components', () => ({
   ...jest.requireActual('@folio/stripes/smart-components'),
   LocationLookup: () => <div>LocationLookup</div>,
   ViewMetaData: () => <div>ViewMetaData</div>,
+  StripesConnectedSource: (props, logger, resourceName) => ({
+    fetchMore: jest.fn(val => val),
+    totalCount: () => props?.resources?.[resourceName]?.other?.totalRecords ?? undefined,
+    update: jest.fn(),
+    loaded: jest.fn(),
+    records: () => props?.resources?.[resourceName]?.records ?? []
+  })
 }), { virtual: true });
