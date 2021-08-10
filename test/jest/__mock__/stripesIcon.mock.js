b30ee4622ced47afad1dec374e138db18840227f
aditya matukumalli <aditya.matukumalli@gmail.com>
2021-08-10 13:51:33 -0400

    Icon mock tweak (#405)
    * Extend IfPermisison mock
    
    * tweak stripesIcon mock
    
    * wait for the initialization of Final form, refs erm-825
    
    * Update OrganizationsFieldArray-test.js

--- test/jest/__mock__/stripesIcon.mock.js
+++ test/jest/__mock__/stripesIcon.mock.js
@@ -3,7 +3,6 @@ import React from 'react';
 jest.mock('@folio/stripes-components/lib/Icon', () => {
   return ({ children }) => (
     <span>
-      Icon
       <span>{children}</span>
     </span>
   );
