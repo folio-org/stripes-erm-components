c1c6e779288c315f7cdc80fc226a2185adc6af83
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-12-02 10:07:17 +0000

    Update smart components mock file

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -7,6 +7,7 @@ jest.mock('@folio/stripes/smart-components', () => ({
   NotesSmartAccordion: () => <div>NotesSmartAccordion</div>,
   ControlledVocab: () => <div>ControlledVocab</div>,
   ConfigManager: () => <div>ConfigManager</div>,
+  NoteCreatePage: () => <div>NoteCreatePage</div>,
   StripesConnectedSource: (props, logger, resourceName) => ({
     fetchMore: jest.fn(val => val),
     totalCount: () => props?.resources?.[resourceName]?.other?.totalRecords ?? undefined,
