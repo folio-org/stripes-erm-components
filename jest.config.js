1a578be37b731749acac4371f5175e58bc95e0ce
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-07-12 15:56:53 +0100

    build: dependency updates
    * Updated some more dependencies and tweaked tests so that they run smoothly
    
    ERM-2151

--- jest.config.js
+++ jest.config.js
@@ -22,4 +22,5 @@ module.exports = {
   testMatch: ['**/(lib|src)/**/?(*.)test.{js,jsx}'],
   testPathIgnorePatterns: ['/node_modules/'],
   setupFilesAfterEnv: [path.join(__dirname, './test/jest/jest.setup.js')],
+  resolver: path.join(__dirname, './test/jest/resolver.js'),
 };
