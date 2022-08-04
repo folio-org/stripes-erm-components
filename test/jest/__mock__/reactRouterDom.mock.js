ac428fdbede1d7fccf945c38986d6c76e51b9bfd
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-08-04 15:32:42 +0100

    test: router mocks
    Added some mock implementations for react-router-dom hooks

--- test/jest/__mock__/reactRouterDom.mock.js
+++ test/jest/__mock__/reactRouterDom.mock.js
@@ -1,4 +1,6 @@
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useLocation: jest.fn().mockReturnValue({}),
+  useHistory: jest.fn(() => ({ push: jest.fn(() => null) })),
+  useParams: jest.fn().mockReturnValue({})
 }));
