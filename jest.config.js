5df205eb4806bf2bfe3056292a28052dab3fd2c9
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-08-07 15:19:08 +0100

    build: Babel dependencies
    Undid last commit, removed some babel deps

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
-const path = require('path');
 const jestConf = require('@folio/stripes-erm-testing/jest.config');
 
 module.exports = {
   ...jestConf,
-  setupFiles: [
-    ...jestConf.setupFiles,
-    path.join(__dirname, './test/jest/jest.setup.js'),
-  ],
 };
