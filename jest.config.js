a9e87fbb8b7cd3dd51beb893204926767d5ae6be
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-08-07 15:50:08 +0100

    build: TEST --- DO NOT MERGE
    Testing transformIgnorePatterns set explicitly -- THIS BELONGS IN ERM-TESTING but am adding here temporarily to check against github actions

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 const jestConf = require('@folio/stripes-erm-testing/jest.config');
 
+// Default config defines some esModules, put any extras specific to our modules here
+const extraESModules = ['@folio', 'ky', '@k-int'].join('|');
+
 module.exports = {
   ...jestConf,
+  transformIgnorePatterns: [
+    `/node_modules/(?!${extraESModules})`
+  ],
 };
