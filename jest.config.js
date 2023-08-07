bde46e3c9e9e76cb6a432841a2c7206d53ccf3fb
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-08-07 16:03:16 +0100

    revert: REVERT TEST ONLY COMMIT
    Have verified the previous commit passes, revert and include change in stripes-erm-testing

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 const jestConf = require('@folio/stripes-erm-testing/jest.config');
 
-// Default config defines some esModules, put any extras specific to our modules here
-const extraESModules = ['@folio', 'ky', '@k-int'].join('|');
-
 module.exports = {
   ...jestConf,
-  transformIgnorePatterns: [
-    `/node_modules/(?!${extraESModules})`
-  ],
 };
