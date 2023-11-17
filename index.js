80f209db0d2d2784f853fa5c3a628128ee66b700
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-11-17 11:41:56 +0000

    refactor: Utilities
    Small refactor to move all utilities into a dedicated utilities directory, and ensure that anything exported from that dir is exported from the library itself as well

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Components
 export { default as ActionMenu } from './lib/ActionMenu';
 export { default as AlternativeNamesFieldArray } from './lib/AlternativeNamesFieldArray';
+export { default as CustomMetaSection } from './lib/CustomMetaSection';
+export { default as DateFilter } from './lib/DateFilter';
 export { default as DocumentCard } from './lib/DocumentCard';
 export { default as DocumentFilter } from './lib/DocumentFilter';
 export { default as DocumentsFieldArray } from './lib/DocumentsFieldArray';
@@ -11,65 +13,32 @@ export { default as EResourceType } from './lib/EResourceType';
 export { default as FileUploader } from './lib/FileUploader';
 export { default as FileUploaderField } from './lib/FileUploaderField';
 export { default as FormattedDateTime } from './lib/FormattedDateTime';
+export { default as InfoBox } from './lib/InfoBox';
 export { default as InternalContactCard } from './lib/InternalContactCard';
 export { default as InternalContactSelection } from './lib/InternalContactSelection';
 export { default as InternalContactsFieldArray } from './lib/InternalContactsFieldArray';
 export { default as OrganizationsFieldArray } from './lib/OrganizationsFieldArray';
-export { default as DateFilter } from './lib/DateFilter';
-export { default as ViewOrganizationCard } from './lib/ViewOrganizationCard';
 export { default as LicenseCard } from './lib/LicenseCard';
 export { default as LicenseEndDate } from './lib/LicenseEndDate';
 export { default as LogsList } from './lib/LogsList';
 export { default as Logs } from './lib/Logs';
+export { default as NewBox } from './lib/NewBox';
 export { default as OrganizationSelection } from './lib/OrganizationSelection';
+export { default as SearchKeyControl } from './lib/SearchKeyControl';
 export { default as SerialCoverage } from './lib/SerialCoverage';
 export { default as Tags } from './lib/Tags';
 export { default as TitleOnPlatformLink } from './lib/TitleOnPlatformLink';
-export { default as CustomMetaSection } from './lib/CustomMetaSection';
-export { default as SearchKeyControl } from './lib/SearchKeyControl';
-
-// Functions, utilities, and misc.
-export { default as generateQueryParams } from './lib/generateQueryParams';
-export { default as getSASParams } from './lib/getSASParams';
-export { default as getSiblingIdentifier } from './lib/getSiblingIdentifier';
-export { default as getResourceIdentifier } from './lib/getResourceIdentifier';
-export { default as isPackage } from './lib/isPackage';
-export { default as renderUserName } from './lib/renderUserName';
-export { default as getRefdataValuesByDesc } from './lib/getRefdataValuesByDesc';
-export { default as renderDynamicRows } from './lib/renderDynamicRows';
-export { default as downloadBlob } from './lib/downloadBlob';
-export { default as recursiveUrlDecoding } from './lib/recursiveUrlDecoding';
-export { default as selectifyRefdata } from './lib/selectifyRefdata';
-
-export {
-  tagsPath,
-  defaultTagsParams,
-  defaultTagQuery,
-  tagNamespaceArray
-} from './lib/tagsConfig'; // Tag configuration for consistency across ERM apps
-
-export { default as preventResourceRefresh } from './lib/preventResourceRefresh';
-export {
-  composeValidators,
-  composeValidatorsWithArgs,
-  invalidNumber as invalidNumberValidator,
-  rangeOverflow,
-  rangeUnderflow,
-  required as requiredValidator,
-  requiredObject as requiredObjectValidator,
-} from './lib/validators';
-
-export { default as handleSaveKeyCommand } from './lib/keyboardShortcutHandlers';
+export { default as ViewOrganizationCard } from './lib/ViewOrganizationCard';
 
 // Shared registry components/functions
 export { default as InternalContactsArrayDisplay } from './lib/InternalContactsArrayDisplay';
 export { default as OrganizationsArrayDisplay } from './lib/OrganizationsArrayDisplay';
-
-export { default as InfoBox } from './lib/InfoBox';
-export { default as NewBox } from './lib/NewBox';
 
 // customHooks
 export * from './lib/hooks';
 
 // constants
 export * from './lib/constants';
+
+// utility functions
+export * from './lib/utils';
