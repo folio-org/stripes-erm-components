6d44aa4d7d69c25b30da0ec02c7eb2c54c60811e
MonireRasouli <monireh.rasouli@k-int.co.uk>
2024-11-05 15:01:55 +0000

    extend DocumentFilter tests

---
+++ test/jest/resources/categoryValues.js
@@ -0,0 +1,19 @@
+const categoryValues = [
+  {
+    'id': '2c9180a09262108601926219be050022',
+    'value': 'consortium_negotiation_document',
+    'label': 'Consortium negotiation document',
+  },
+  {
+    'id': '2c9180a09262108601926219bdfc0020',
+    'value': 'license',
+    'label': 'License',
+  },
+  {
+    'id': '2c9180a09262108601926219be010021',
+    'value': 'misc',
+    'label': 'Misc',
+  },
+];
+
+export default categoryValues;
---
+++ test/jest/resources/index.js
@@ -0,0 +1,2 @@
+export { default as categoryValues } from './categoryValues';
+
